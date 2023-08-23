ac81638402244228d8f58ceb28dec3fcc9adfffc
Daniel Clavijo Coca <dclavijo@opennebula.io>
2023-08-23 12:07:43 -0600

    WIP Translation

--- share/redirect.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-one="172.20.0.8"
-executor="one@supermicro9"
-
-ssh -v -N -L 1338:$one:2633 $executor
-ssh -v -N -L 1339:$one:2474 $executor
-ssh -v -N -L 9869:$one:9869 $executor
