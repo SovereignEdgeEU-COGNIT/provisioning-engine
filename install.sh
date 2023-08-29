36cd455a5531ffc33fbe9b72268e6cd402dd85be
Daniel Clavijo Coca <dclavijo@opennebula.io>
2023-08-29 16:23:13 -0600

    installer copy mode and purge uninstall mode

--- install.sh
+++ install.sh
@@ -1,58 +1,78 @@
 #!/usr/bin/env bash
 
-# TODO: Install modes, symlink and copy. symlinked used now
 install() {
 	for gem_name in "${gems[@]}"; do
 		is_gem_installed "$gem_name" >/dev/null 2>&1 || gem install "$gem_name"
 	done
 
 	[ -d "$CONF_DIR" ] || sudo mkdir "$CONF_DIR"
-	[ -L "$CONF_PATH" ] || sudo ln -s "$(realpath "share/etc/$CONF_FILE")" "$CONF_DIR"
-	[ -d "$install_dir" ] || mkdir "$install_dir"
+	[ -d "$INSTALL_DIR" ] || mkdir "$INSTALL_DIR"
+
+	if [[ $setup_mode == "copy" ]]; then
+		[ -f "$CONF_PATH" ] || sudo cp "$(realpath "share/etc/$CONF_FILE")" "$CONF_DIR"
+	elif [[ $setup_mode == "symlink" ]]; then
+		[ -L "$CONF_PATH" ] || [ -f "$CONF_PATH" ] || sudo ln -s "$(realpath "share/etc/$CONF_FILE")" "$CONF_DIR"
+	fi
 
 	for file in $modules; do
-		dst="$install_dir/${file}"
+		dst="$INSTALL_DIR/${file}"
 
-		[ -L "$dst" ] || ln -s "$(realpath "src/server/${file}")" "${install_dir}/"
+		if [[ $setup_mode == "copy" ]]; then
+			[ -L "$dst" ] || cp "$(realpath "src/server/${file}")" "${INSTALL_DIR}/"
+		elif [[ $setup_mode == "symlink" ]]; then
+			[ -L "$dst" ] || ln -s "$(realpath "src/server/${file}")" "${INSTALL_DIR}/"
+		fi
 	done
 
 	# server.rb is installed as the executable
-	[ -L "$EXEC_FILE" ] || sudo ln -s "${install_dir}/server.rb" "$EXEC_FILE"
+	[ -L "$EXEC_FILE" ] || sudo ln -s "${INSTALL_DIR}/server.rb" "$EXEC_FILE"
 }
 
 postinstall() {
-	[ -L "$EXEC_FILE" ] || sudo ln -s "${install_dir}/server.rb" "$EXEC_FILE"
-	echo "provision engine installed at ${install_dir}"
+	[ -L "$EXEC_FILE" ] || sudo ln -s "${INSTALL_DIR}/server.rb" "$EXEC_FILE"
+	echo "provision engine installed at ${INSTALL_DIR}"
 	echo "run engine with ${EXEC_FILE} start/stop"
 }
 
-# TODO: Make thorough clean
 clean() {
-	echo "${CONF_DIR} will not be deleted as part of the cleanup process"
+	if [[ $setup_mode == "purge" ]]; then
+		[ -d $CONF_DIR ] && sudo rm -r $CONF_DIR
 
-	[ -L $CONF_PATH ] && sudo rm $CONF_PATH
-	[ -d "$install_dir" ] && rm -rf "$install_dir"
-	[ -L "$EXEC_FILE" ] && sudo rm $EXEC_FILE
+		for gem_name in "${gems[@]}"; do
+			remove_orphan_gem "$gem_name"
+		done
 
-	# for gem_name in "${gems[@]}"; do
-	# 	is_gem_installed "$gem_name" && gem uninstall "$gem_name"
-	# done
+		[ -f "$EXEC_FILE" ] && sudo rm $EXEC_FILE
+	else
+		echo "${CONF_DIR} will not be deleted as part of the cleanup process"
+		echo "The ruby gems: $(printf "%s " "${gems[@]}") are required by the provision engine"
+
+		[ -f "$CONF_PATH" ] && sudo rm $CONF_PATH
+		[ -L "$EXEC_FILE" ] && sudo rm $EXEC_FILE
+	fi
+
+	[ -d "$INSTALL_DIR" ] && rm -rf "$INSTALL_DIR"
 }
 
 is_gem_installed() {
 	gem list -i "$1" >/dev/null 2>&1
 }
 
+remove_orphan_gem() {
+	gem uninstall -a --abort-on-dependent "$1" >/dev/null 2>&1
+}
+
 CONF_DIR="/etc/provision-engine"
 CONF_FILE="engine.conf"
 CONF_PATH="${CONF_DIR}/${CONF_FILE}"
 EXEC_FILE="/usr/local/bin/provision-engine-server"
+INSTALL_DIR="/opt/provision-engine" # install location is hardcoded
 
 modules="client.rb configuration.rb log.rb server.rb runtime.rb"
 gems=("opennebula" "sinatra" "logger" "json-schema") # check requires on server.rb
 
 action="${1:-"install"}"
-install_dir="${2:-"/opt/provision-engine"}"
+setup_mode="${2:-"symlink"}" # TODO: change to copy once is production ready
 
 if [ "$action" = "clean" ]; then
 	clean
