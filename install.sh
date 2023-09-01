31b518ab62e3d6cfb691d1f80305f7dd5a135ce6
Daniel Clavijo Coca <dclavijo@opennebula.io>
2023-09-01 16:36:06 -0600

    fix installer

--- install.sh
+++ install.sh
@@ -8,30 +8,30 @@ install() {
 	[ -d "$CONF_DIR" ] || sudo mkdir "$CONF_DIR"
 	[ -d "$INSTALL_DIR" ] || mkdir "$INSTALL_DIR"
 
+	src_conf="$(realpath "share/etc/$CONF_FILE")"
+	src_exec="$(realpath src/server/cli/provision-engine-server)"
+
 	if [[ $setup_mode == "copy" ]]; then
-		[ -f "$CONF_PATH" ] || sudo cp "$(realpath "share/etc/$CONF_FILE")" "$CONF_DIR"
+		[ -f "$CONF_PATH" ] || sudo cp "$src_conf" "$CONF_PATH"
+		[ -L "$EXEC_PATH" ] || sudo cp "$src_exec" "$EXEC_PATH"
+
+		for file in $modules; do
+			[ -L "$INSTALL_DIR/${file}" ] || cp "$(realpath "src/server/${file}")" "${INSTALL_DIR}"
+		done
 	elif [[ $setup_mode == "symlink" ]]; then
-		[ -L "$CONF_PATH" ] || [ -f "$CONF_PATH" ] || sudo ln -s "$(realpath "share/etc/$CONF_FILE")" "$CONF_DIR"
+		[ -L "$CONF_PATH" ] || [ -f "$CONF_PATH" ] || sudo ln -s "$src_conf" "$CONF_PATH"
+		[ -L "$EXEC_PATH" ] || sudo ln -s "$src_exec" "$EXEC_PATH"
+
+		for file in $modules; do
+			[ -L "$INSTALL_DIR/${file}" ] || ln -s "$(realpath "src/server/${file}")" "${INSTALL_DIR}"
+		done
 	fi
-
-	for file in $modules; do
-		dst="$INSTALL_DIR/${file}"
-
-		if [[ $setup_mode == "copy" ]]; then
-			[ -L "$dst" ] || cp "$(realpath "src/server/${file}")" "${INSTALL_DIR}/"
-		elif [[ $setup_mode == "symlink" ]]; then
-			[ -L "$dst" ] || ln -s "$(realpath "src/server/${file}")" "${INSTALL_DIR}/"
-		fi
-	done
-
-	# server.rb is installed as the executable
-	[ -L "$EXEC_FILE" ] || sudo ln -s "${INSTALL_DIR}/server.rb" "$EXEC_FILE"
 }
 
 postinstall() {
-	[ -L "$EXEC_FILE" ] || sudo ln -s "${INSTALL_DIR}/server.rb" "$EXEC_FILE"
+	[ -L "$EXEC_PATH" ] || sudo ln -s "${INSTALL_DIR}/server.rb" "$EXEC_PATH"
 	echo "provision engine installed at ${INSTALL_DIR}"
-	echo "run engine with ${EXEC_FILE} start/stop"
+	echo "run engine with ${EXEC_PATH} start/stop"
 }
 
 clean() {
@@ -41,17 +41,14 @@ clean() {
 		for gem in "${gems[@]}"; do
 			remove_orphan_gem "$gem"
 		done
-
-		[ -d "$INSTALL_DIR" ] && rm -rf "$INSTALL_DIR"
-		[ -f "$EXEC_FILE" ] && sudo rm $EXEC_FILE
 	else
 		echo "${CONF_DIR} will not be deleted as part of the cleanup process"
 		echo "The ruby gems: $(printf "%s " "${gems[@]}") are required by the provision engine"
-
-		[ -f "$CONF_PATH" ] && sudo rm $CONF_PATH
-		[ -L "$EXEC_FILE" ] && sudo rm $EXEC_FILE
 	fi
 
+	[ -f "$EXEC_PATH" ] && sudo rm $EXEC_PATH
+	[ -L "$EXEC_PATH" ] && sudo rm $EXEC_PATH
+	[ -d "$INSTALL_DIR" ] && rm -rf "$INSTALL_DIR"
 }
 
 is_gem_installed() {
@@ -65,7 +62,8 @@ remove_orphan_gem() {
 CONF_DIR="/etc/provision-engine"
 CONF_FILE="engine.conf"
 CONF_PATH="${CONF_DIR}/${CONF_FILE}"
-EXEC_FILE="/usr/local/bin/provision-engine-server"
+EXEC_FILE="provision-engine-server"
+EXEC_PATH="/usr/local/bin/${EXEC_FILE}"
 INSTALL_DIR="/opt/provision-engine" # install location is hardcoded
 
 modules="client.rb configuration.rb log.rb server.rb runtime.rb"
