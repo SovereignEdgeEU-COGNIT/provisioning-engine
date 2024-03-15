48e7f0805fa1f145dc23451bf28ff106603936f5
Daniel Clavijo Coca <dclavijo@opennebula.io>
2024-03-15 03:08:00 -0600

    Remove unnecesary check (#42)

--- src/server/server.rb
+++ src/server/server.rb
@@ -25,7 +25,7 @@ require 'error'
 require 'runtime'
 require 'function'
 
-VERSION = '1.3.1'
+VERSION = '1.3.2'
 
 ############################################################################
 # API configuration
